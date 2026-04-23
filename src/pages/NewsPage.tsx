9efff4ca29bfd5e6e05da4a9c186d3d75dbab67f
Peeyush <peeyushp00@gmail.com>
2026-04-23 17:39:25 +0530

    fix news feed niche filter

--- src/pages/NewsPage.tsx
+++ src/pages/NewsPage.tsx
@@ -57,6 +57,11 @@ if (defaultQuery) {
   const filtered = filterArticlesLogic(list, defaultQuery);
   // If no results found, show all articles
   setFilteredArticles(filtered.length > 0 ? filtered : list);
+// If no niche match found, clear search so user sees all news
+if (filtered.length === 0 && defaultQuery === userNiche) {
+  setQuery('');
+  setSearchInput('');
+}
 } else {
   setFilteredArticles(list);
 }
